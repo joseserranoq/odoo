f49575ff2f4a3817f6402327577051a0ef59be5a
Jose Serrano Quesada <joseserranoq00@gmail.com>
2025-11-03 14:47:36 -0600

    Chapter 10

--- modules/real_estate/__manifest__.py
+++ modules/real_estate/__manifest__.py
@@ -21,8 +21,13 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        'views/estate_menus.xml',
+        'security/ir.model.access.csv',        
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
+        
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ modules/real_estate/models/estate_property_type.py
@@ -0,0 +1,18 @@
+from odoo import models, fields,exceptions, api
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Real Estate Property Type'
+
+    name = fields.Char(string="Type Name", required=True)
+    # property_type_id = fields.Char(string="Property Type", required=True)
+    # buyer_id = fields.Many2one('estate.property', string="Buyer")
+    # seller_id = fields.Many2one('estate.property', string="Seller")
+
+    #constraint
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            existing_type = self.search([('name', '=', record.name), ('id', '!=', record.id)])
+            if existing_type:
+                raise exceptions.ValidationError("The property type name must be unique.")
